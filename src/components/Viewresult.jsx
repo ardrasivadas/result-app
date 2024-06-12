7b1060a12e53941a21e8565abcd3aa86f62b721f
ardrasivadas <ardrac15@gmail.com>
2024-06-13 00:52:26 +0530

    created

--- src/components/Viewresult.jsx
+++ src/components/Viewresult.jsx
@@ -21,11 +21,27 @@ const Viewresult = () => {
                 <tbody>
                     <tr>
 
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>Mark</td>
-                        <td>Mark</td>
-                        <td>@mdo</td>
+                        <td>Malavika</td>
+                        <td>102</td>
+                        <td>4th</td>
+                        <td>97</td>
+                        <td>A+</td>
+                    </tr>
+                    <tr>
+
+                        <td>Arun</td>
+                        <td>109</td>
+                        <td>7th</td>
+                        <td>88</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+
+                        <td>Rinu</td>
+                        <td>124</td>
+                        <td>9th</td>
+                        <td>76</td>
+                        <td>B+</td>
                     </tr>
 
 
