bc0ef749f1f32fe46ac22e2f1fb09470b61b1a64
ardrasivadas <ardrac15@gmail.com>
2024-06-12 22:06:17 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Addresult from './components/Addresult';
+import Search from './components/Search';
+import Viewall from './components/Viewall';
 
 function App() {
   return (
     <div className="App">
       <Addresult/>
+      <Search/>
+      <Viewall/>
     </div>
   );
 }
---
+++ src/components/Viewall.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Viewall = () => {
+  return (
+    <div>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">NAME</th>
+      <th scope="col">ADMISSION NO</th>
+      <th scope="col">CLASS</th>
+      <th scope="col">MARK</th>
+      <th scope="col">GRADE</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>@mdo</td>
+    </tr>
+    
+    
+  </tbody>
+</table>
+
+    </div>
+  )
+}
+
+export default Viewall
