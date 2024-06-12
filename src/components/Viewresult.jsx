662dc08f1eeb3c05a64007125e9eb97d7b30b8ef
ardrasivadas <ardrac15@gmail.com>
2024-06-13 01:36:49 +0530

    created

--- src/components/Viewresult.jsx
+++ src/components/Viewresult.jsx
@@ -1,60 +1,66 @@
 import React from 'react'
 
 const Viewresult = () => {
-  return (
-    <div>
 
-<div className="container">
-    <div className="row">
-        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
-        <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">NAME</th>
-                        <th scope="col">ADMISSION NO</th>
-                        <th scope="col">CLASS</th>
-                        <th scope="col">MARK</th>
-                        <th scope="col">GRADE</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-
-                        <td>Malavika</td>
-                        <td>102</td>
-                        <td>4th</td>
-                        <td>97</td>
-                        <td>A+</td>
-                    </tr>
-                    <tr>
-
-                        <td>Arun</td>
-                        <td>109</td>
-                        <td>7th</td>
-                        <td>88</td>
-                        <td>A</td>
-                    </tr>
-                    <tr>
-
-                        <td>Rinu</td>
-                        <td>124</td>
-                        <td>9th</td>
-                        <td>76</td>
-                        <td>B+</td>
-                    </tr>
+    const [data, changeData] = useState([])
+    const fetchData = () => {
+        axios.get("http://localhost:8080/view").then(
+            (Response) => {
+                changeData(Response.data)
+            }
+        ).catch()
+    }
+    useEffect(() => { fetchData() }, [])
 
 
-                </tbody>
-            </table>
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">NAME</th>
+                                    <th scope="col">ADMISSION NO</th>
+                                    <th scope="col">CLASS</th>
+                                    <th scope="col">MARK</th>
+                                    <th scope="col">GRADE</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                {data.map(
+                                    (value, index) => {
+
+                                        return <tr>
+                                            <td>{value.name}</td>
+                                            <td>{value.admno}</td>
+                                            <td>{value.class}</td>
+                                            <td>{value.mark}</td>
+                                            <td>{value.grade}</td>
+                                        </tr>
+                                    }
+                                )}
+
+
+                                
+
+                                    
+
+
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+
 
         </div>
-    </div>
-</div>
-
-
-    </div>
-  )
+    )
 }
 
 export default Viewresult
