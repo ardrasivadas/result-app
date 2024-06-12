814d717b7b4f40ddd6296c99820ee15828761039
ardrasivadas <ardrac15@gmail.com>
2024-06-12 22:48:23 +0530

    created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Addresult from './components/Addresult';
 import Search from './components/Search';
+import Viewall from './components/Viewall';
 
 function App() {
   return (
     <div className="App">
       <Addresult/>
       <Search/>
+      <Viewall/>
     </div>
   );
 }
